a582ed4b4e7360e57068ec3590371b93351ac92e
Bruno Amorim <bruno.amorim@mblabs.com.br>
2023-09-27 22:02:15 -0300

    Initial Commit

---
+++ Login Screen/Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ScreenApp.swift
+//  Login Screen
+//
+//  Created by Bruno Amorim on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
